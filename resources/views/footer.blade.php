168fe73e385b3ba4f859b0a6f8cf3872a9d320fb
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-10-24 19:36:02 +0700

    source code landing page furniture

--- resources/views/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer" style="bottom:0">
-    <div class="container">
-        <center><span>All Rights Reserved</span></center>
-        <div class="credits">
-            <a>Bootstrap UI</a> by <a href="https://bootstrapmade.com/">BootstrapFrontEnd</a>
-        </div>
-    </div>
-</footer>
